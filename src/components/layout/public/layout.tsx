7afc68dea15b9460aa203716ff73991c82a494dc
Hanif Radityo <radityo.hanif@gmail.com>
2024-12-15 23:08:15 +0700

    add favicon

--- src/components/layout/public/layout.tsx
+++ src/components/layout/public/layout.tsx
@@ -22,6 +22,8 @@ export default function PublicLayout({ children }: Props) {
         <title>{title}</title>
         <meta name="description" content={title} />
         <meta name="keywords" content={keywords} />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
       </Helmet>
       <Navbar />
       {children}
